eb8d76a59b7d79ad54c9cd0cf1bf25a049ceacb0
Mau1401 <mauricio.rodriguezobando@ucr.ac.cr>
2025-06-14 13:47:58 -0600

    Contract Basic Structure

--- Contracts/KipuBank.sol
+++ Contracts/KipuBank.sol
@@ -1,2 +1,93 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
+
+/** 
+ * @title KIPU bank contract
+ */
+contract KipuBank {
+    // ============== STATE VARS ==============
+    // ---- Immutable ---- 
+    uint256 public immutable thresholdWithdrawl;
+    uint256 public immutable bankCap; // Deposit global cap
+
+    // ---- Storage ---- 
+    uint256 public totalDeposits;
+    uint256 public depositCount;
+    uint256 public withdrawalCount;
+
+    // ============== Mappings ==============
+    mapping(address => uint256) public vault;
+
+    // ============== ERRORS ==============
+    error ExceedsBankCap(); 
+    error ExceedsThresholdWithdrawl();
+    error InsufficientVaultBalance();
+    error InvalidThreshold();
+    error ZeroAmountNotAllowed();
+
+    // ============== EVENTS ==============
+    event NewDeposit();
+    event NewWithdrawal();
+
+    // ============== CONSTRUCTOR ==============
+    constructor(uint256 _bankCap, uint256 _thresholdWithdrawl) {
+        // Init 
+        bankCap = _bankCap;
+        thresholdWithdrawl = _thresholdWithdrawl;
+    }
+
+    // ============== MODDIFIERS ==============
+    modifier checkBankCap(){
+        _;
+    }
+
+    modifier validAmount(){
+        _;
+    }
+
+    // ============== FUNCTIONS ==============
+    // ---- Main ----
+     /*
+     * @notice 
+     * @dev 
+     * @param
+     * @return
+     */
+    function deposit() external payable validAmount checkBankCap {
+        // ... deposit logic ...
+        emit NewDeposit();
+    }
+
+    /*
+     * @notice 
+     * @dev 
+     * @param
+     * @return
+     */
+    function withdraw() external validAmount {
+         // ... whitdraw logic ...
+        _updateTotalAfterWithdraw();
+        emit NewWithdrawal();
+    }
+
+    /*
+     * @notice 
+     * @dev 
+     * @param
+     * @return
+     */
+    function _updateTotalAfterWithdraw() private {
+    
+    } 
+
+    // ---- Aux ----
+    /*
+     * @notice 
+     * @dev 
+     * @param
+     * @return
+     */
+    function getVaultBalance() external view returns (uint256) {
+
+    }
+}
